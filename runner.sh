408f31da927ad82e75a1efc7bcf105254a135bb3
Nik Krumm <nkrumm@gmail.com>
2019-09-09 21:06:55 -0700

    Get IAM credentials via injected env vars from task definition

--- runner.sh
+++ runner.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI > creds.json
-
-cat creds.json
-
-export AWS_ACCESS_KEY_ID=`jq -r .AccessKeyId creds.json`
-export AWS_SECRET_ACCESS_KEY=`jq -r .SecretAccessKey creds.json`
-export AWS_SESSION_TOKEN=`jq -r .Token creds.json`
-
-rm creds.json
+echo $AWS_ACCESS_KEY_ID
 
 aws s3 cp $1 .
 aws s3 cp $2 .

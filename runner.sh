5b9a80b1d499e4f9aa6584519dbeb9e735179696
nkrumm <nkrumm@uw.edu>
2019-10-02 15:29:45 -0700

    pass $NEXTFLOW_OPTIONS to nextflow

--- runner.sh
+++ runner.sh
@@ -2,8 +2,9 @@
 
 echo $AWS_ACCESS_KEY_ID
 echo $BATCHMAN_LOG_ENDPOINT
+echo $NEXTFLOW_OPTIONS
 
 aws s3 cp $1 .
 aws s3 cp $2 .
 
-nextflow run main.nf -config nextflow.config -with-weblog $BATCHMAN_LOG_ENDPOINT
+nextflow run main.nf -config nextflow.config -with-weblog $BATCHMAN_LOG_ENDPOINT $NEXTFLOW_OPTIONS

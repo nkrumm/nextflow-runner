680aa47fd83882be6ce3ecce2129535da1f406bc
nkrumm <nkrumm@uw.edu>
2019-10-02 09:15:50 -0700

    Add -with-weblog command to nextflow in runner.sh

--- runner.sh
+++ runner.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo $AWS_ACCESS_KEY_ID
+echo $BATCHMAN_LOG_ENDPOINT
 
 aws s3 cp $1 .
 aws s3 cp $2 .
 
-nextflow run main.nf -config nextflow.config
+nextflow run main.nf -config nextflow.config -with-weblog $BATCHMAN_LOG_ENDPOINT

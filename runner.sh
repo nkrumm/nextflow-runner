001b6630382ebc5f52cc0da8e791f28a8c5c8d7b
Nik Krumm <nkrumm@gmail.com>
2020-03-31 20:58:58 -0700

    Add back in params.json sync

--- runner.sh
+++ runner.sh
@@ -6,7 +6,6 @@ TASK_ARN=$(curl --silent ${ECS_CONTAINER_METADATA_URI}/task | jq -r '.TaskARN' |
 WEBLOG_ENDPOINT="${API_ENDPOINT}/api/v1/weblog?taskArn=${TASK_ARN}"
 
 echo "EXECUTION_TYPE    = ${EXECUTION_TYPE}"
-echo "AWS_ACCESS_KEY_ID = ${AWS_ACCESS_KEY_ID}"
 echo "API_ENDPOINT      = ${API_ENDPOINT}"
 echo "API_KEY           = ${API_KEY}"
 echo "TASK_ARN          = ${TASK_ARN}"
@@ -14,12 +13,24 @@ echo "WEBLOG_ENDPOINT   = ${WEBLOG_ENDPOINT}"
 echo "NEXTFLOW_OPTIONS  = ${NEXTFLOW_OPTIONS}"
 
 if [ ! -z "$NF_SESSION_CACHE_ARN" ]; then
-	NF_SESSION_CACHE_DIR_IN="${NF_SESSION_CACHE_DIR}/${NF_SESSION_CACHE_ARN}"
-	echo "NF_SESSION_CACHE_ARN is set, will attempt -resume with cache from ${NF_SESSION_CACHE_DIR_IN}"
+   NF_SESSION_CACHE_DIR_IN="${NF_SESSION_CACHE_DIR}/${NF_SESSION_CACHE_ARN}"
+   echo "NF_SESSION_CACHE_ARN is set, will attempt -resume with cache from ${NF_SESSION_CACHE_DIR_IN}"
 fi
 # this needs to be set regardless, to save current workflow .nextflow/
 NF_SESSION_CACHE_DIR_OUT="${NF_SESSION_CACHE_DIR}/${TASK_ARN}"
 echo $NF_SESSION_CACHE_DIR_OUT # this is a new directory based on current ARN
+
+# stage in session cache
+# .nextflow directory holds all session information for the current and past runs.
+# We separately store .nextflow/ directories by the Fargate Task ARN, which means
+# that restarts can be identified by the prior Task ARN.
+# it should be `sync`'d with an s3 uri, so that runs from previous sessions can be 
+# resumed
+# taken from https://docs.opendata.aws/genomics-workflows/orchestration/nextflow/nextflow-overview/
+if [ ! -z "$NF_SESSION_CACHE_DIR_IN" ]; then
+    echo "== Restoring Session Cache =="
+    aws s3 sync --only-show-errors $NF_SESSION_CACHE_DIR_IN/.nextflow .nextflow
+fi
 
 
 # Stage nextflow files or repository
@@ -49,16 +60,11 @@ case $EXECUTION_TYPE in
         ;;
 esac
 
-# stage in session cache
-# .nextflow directory holds all session information for the current and past runs.
-# We separately store .nextflow/ directories by the Fargate Task ARN, which means
-# that restarts can be identified by the prior Task ARN.
-# it should be `sync`'d with an s3 uri, so that runs from previous sessions can be 
-# resumed
-# taken from https://docs.opendata.aws/genomics-workflows/orchestration/nextflow/nextflow-overview/
-if [ ! -z "$NF_SESSION_CACHE_DIR_IN" ]; then
-	echo "== Restoring Session Cache =="
-	aws s3 sync --only-show-errors $NF_SESSION_CACHE_DIR_IN/.nextflow .nextflow
+
+# if provided, download a params.json file which will override params in the config.
+if [ ! -z "$NF_PARAMS_FILE" ]; then
+    echo "== Downloading nextflow params file =="
+    aws s3 cp $NF_PARAMS_FILE .
 fi
 
 function preserve_session() {
